7dc3cd75f8d8a3293bb425ad7f8a3c0e1489d3ce
Carlos Alvarado Robles <carlos2000emilioa@gmail.com>
2022-11-24 12:16:53 -0500

    Mejorando los estilos de los componentes

--- resources/views/ficha/index.blade.php
+++ resources/views/ficha/index.blade.php
@@ -14,12 +14,8 @@
                         {{-- mensaje si se creo por primera vez --}}
                         <div class="py-2 flex justify-between flex-col md:flex-row ">
                             <h3 class="text-xl flex gap-1 font-bold text-gray-700">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 01-1.125-1.125v-3.75zM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-8.25zM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-2.25z" />
-                                </svg>
-                                Lista de Fichas Registradas
+                                
+                                <i class="fa-sharp fa-solid fa-file"></i> Lista de Fichas Registradas
                             </h3>
                             <!--<a href="#"
                                     class="mt-2 md:mt-0 flex items-center border rounded-lg  bg-blue-700 hover:bg-blue-800 transition-colors text-white text-sm  p-2 cursor-pointer  w-full md:w-auto first-letter:uppercase ">
@@ -31,7 +27,7 @@
 
                         </div>
 
-                        <div class="flex">
+                        <!--<div class="flex">
                             <div class="w-1/4
                             p-4">
                                 <div class="flex items-center">
@@ -68,15 +64,67 @@
                             </div>
                             <div class="w-1/3 p-4">
                                 <div class="flex items-end">
-                                    <a href="{{ route('ficha.create') }}" type="button"
+                                    <a href="{{-- route('ficha.create') --}}" type="button"
                                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Nuevo
                                         Registro</a>
                                 </div>
 
                             </div>
 
-                        </div>
+                        </div>-->
 
+                        <!--<div class="grid grid-cols-3 gap-4">
+                            <div class="bg-blue-600">01</div>
+                            <div class="bg-gray-200">02</div>
+                            <div class="bg-gray-200">03</div>
+                            <div class="col-span-2 bg-blue-800">04</div>
+                            <div class="bg-gray-200">05</div>
+                            <div class="...">06</div>
+                            <div class="col-span-2 ...">07</div>
+                        </div>-->
+
+                        <div class="grid grid-cols-6 gap-4">
+                            <div class="col-span-2">
+                                <div class="flex items-center">
+                                    <span>Mostrar</span>
+                                    <select id="countries"
+                                        class="mx-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                        <option selected>10</option>
+                                        <option value="US">25</option>
+                                        <option value="CA">100</option>
+                                        <option value="FR">Todo</option>
+                                    </select>
+                                    <span>registros</span>
+                                </div>
+
+                            </div>
+                            <div class="col-span-3">
+                                <div class="flex items-center">
+                                    <div class="relative w-full">
+                                        <div
+                                            class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                                                fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                                            </svg>
+                                        </div>
+                                        <input type="search" id="default-search"
+                                            class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                            placeholder="Buscar ..." required>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-span-1">
+                                <div class="flex items-end">
+                                    <a href="{{ route('ficha.create') }}" type="button"
+                                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                                        <i class="fa-regular fa-file"></i> Nuevo
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
 
                         <div class="overflow-x-auto relative shadow-md sm:rounded-lg mt-4">
                             <table class="table-auto w-full text-sm text-left text-gray-500 dark:text-gray-400">
