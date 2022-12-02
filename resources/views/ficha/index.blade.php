e90aa3b25b365a93273cb8493e22e763a1b9cfd8
Carlos Orellano <orellano428@gmail.com>
2022-12-01 22:48:08 -0500

    diseño en Lista de Fichas

--- resources/views/ficha/index.blade.php
+++ resources/views/ficha/index.blade.php
@@ -5,125 +5,58 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-5">
 
         <div class="p-5">
-            <div class="max-w-5xl mx-auto sm:px-6 lg:px-8 ">
+            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 ">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
                         {{-- mensaje si se creo por primera vez --}}
                         <div class="py-2 flex justify-between flex-col md:flex-row ">
                             <h3 class="text-xl flex gap-1 font-bold text-gray-700">
-                                
+
                                 <i class="fa-sharp fa-solid fa-file"></i> Lista de Fichas Registradas
                             </h3>
-                            <!--<a href="#"
-                                    class="mt-2 md:mt-0 flex items-center border rounded-lg  bg-blue-700 hover:bg-blue-800 transition-colors text-white text-sm  p-2 cursor-pointer  w-full md:w-auto first-letter:uppercase ">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                        stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                    </svg> Nuevo Material</a>-->
-
-                        </div>
-
-                        <!--<div class="flex">
-                            <div class="w-1/4
-                            p-4">
-                                <div class="flex items-center">
-                                    <span>Mostrar</span>
-                                    <select id="countries"
-                                        class="mx-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                        <option selected>10</option>
-                                        <option value="US">25</option>
-                                        <option value="CA">100</option>
-                                        <option value="FR">Todo</option>
-                                    </select>
-                                    <span>registros</span>
-                                </div>
-
-                            </div>
-                            <div class="w-1/3 
-                            p-4">
-                                <div class="flex items-center">
-                                    <div class="relative">
-                                        <div
-                                            class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400"
-                                                fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                                xmlns="http://www.w3.org/2000/svg">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                                            </svg>
-                                        </div>
-                                        <input type="search" id="default-search"
-                                            class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                            placeholder="Buscar ..." required>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="w-1/3 p-4">
-                                <div class="flex items-end">
-                                    <a href="{{-- route('ficha.create') --}}" type="button"
-                                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Nuevo
-                                        Registro</a>
-                                </div>
-
-                            </div>
-
-                        </div>-->
-
-                        <!--<div class="grid grid-cols-3 gap-4">
-                            <div class="bg-blue-600">01</div>
-                            <div class="bg-gray-200">02</div>
-                            <div class="bg-gray-200">03</div>
-                            <div class="col-span-2 bg-blue-800">04</div>
-                            <div class="bg-gray-200">05</div>
-                            <div class="...">06</div>
-                            <div class="col-span-2 ...">07</div>
-                        </div>-->
-
-                        <div class="grid grid-cols-6 gap-4">
-                            <div class="col-span-2">
-                                <div class="flex items-center">
-                                    <span>Mostrar</span>
-                                    <select id="countries"
-                                        class="mx-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                        <option selected>10</option>
-                                        <option value="US">25</option>
-                                        <option value="CA">100</option>
-                                        <option value="FR">Todo</option>
-                                    </select>
-                                    <span>registros</span>
-                                </div>
-
-                            </div>
-                            <div class="col-span-3">
-                                <div class="flex items-center">
-                                    <div class="relative w-full">
-                                        <div
-                                            class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400"
-                                                fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                                xmlns="http://www.w3.org/2000/svg">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                                            </svg>
-                                        </div>
-                                        <input type="search" id="default-search"
-                                            class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                            placeholder="Buscar ..." required>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-span-1">
+                            <div class="">
                                 <div class="flex items-end">
                                     <a href="{{ route('ficha.create') }}" type="button"
-                                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-8 py-2.5 < mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                                         <i class="fa-regular fa-file"></i> Nuevo
                                     </a>
                                 </div>
                             </div>
+                        </div>
+
+                        <div class="flex justify-between  items-center mt-3">
+                            <div class="flex items-center w-2/4">
+                                <div class="relative w-full">
+                                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                                            fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                                        </svg>
+                                    </div>
+                                    <input type="search" id="default-search"
+                                        class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="Buscar ..." required>
+                                </div>
+                            </div>
+                            <div class="flex items-center w-1/4">
+                                <span>Mostrar</span>
+                                <select id="countries"
+                                    class="mx-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    <option selected>10</option>
+                                    <option value="US">25</option>
+                                    <option value="CA">100</option>
+                                    <option value="FR">Todo</option>
+                                </select>
+                                <span>registros</span>
+                            </div>
+
+
+
                         </div>
 
                         <div class="overflow-x-auto relative shadow-md sm:rounded-lg mt-4">
@@ -176,9 +109,13 @@
                                         </th>
                                         <th scope="row"
                                             class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                            <a href="#">Editar</a>
-                                            <a href="#" onclick="ejecutarAlert()">Eliminar</a>
-                                            <a href="#">Ver Todo</a>
+                                            <a href="#"
+                                                class="inline-flex items-center px-3 py-1 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">Editar</a>
+                                            <a href="#" onclick="ejecutarAlert()"
+                                                class="inline-flex items-center px-3 py-1 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 active:bg-red-900 focus:outline-none focus:border-red-900 focus:ring focus:ring-red-300 disabled:opacity-25 transition">Eliminar</a>
+                                            <a href="#"
+                                                class="inline-flex items-center px-3 py-1 bg-blue-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 active:bg-blue-900 focus:outline-none focus:border-blue-900 focus:ring focus:ring-blue-300 disabled:opacity-25 transition">Ver
+                                                Todo</a>
                                         </th>
                                     </tr>
                                     <tr
@@ -205,9 +142,13 @@
                                         </th>
                                         <th scope="row"
                                             class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                            <a href="#">Editar</a>
-                                            <a href="#" onclick="ejecutarAlert()">Eliminar</a>
-                                            <a href="#">Ver Todo</a>
+                                            <a href="#"
+                                                class="inline-flex items-center px-3 py-1 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">Editar</a>
+                                            <a href="#" onclick="ejecutarAlert()"
+                                                class="inline-flex items-center px-3 py-1 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 active:bg-red-900 focus:outline-none focus:border-red-900 focus:ring focus:ring-red-300 disabled:opacity-25 transition">Eliminar</a>
+                                            <a href="#"
+                                                class="inline-flex items-center px-3 py-1 bg-blue-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 active:bg-blue-900 focus:outline-none focus:border-blue-900 focus:ring focus:ring-blue-300 disabled:opacity-25 transition">Ver
+                                                Todo</a>
                                         </th>
                                     </tr>
                                     <tr
@@ -234,9 +175,13 @@
                                         </th>
                                         <th scope="row"
                                             class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                            <a href="#">Editar</a>
-                                            <a href="#" onclick="ejecutarAlert()">Eliminar</a>
-                                            <a href="#">Ver Todo</a>
+                                            <a href="#"
+                                                class="inline-flex items-center px-3 py-1 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">Editar</a>
+                                            <a href="#" onclick="ejecutarAlert()"
+                                                class="inline-flex items-center px-3 py-1 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 active:bg-red-900 focus:outline-none focus:border-red-900 focus:ring focus:ring-red-300 disabled:opacity-25 transition">Eliminar</a>
+                                            <a href="#"
+                                                class="inline-flex items-center px-3 py-1 bg-blue-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 active:bg-blue-900 focus:outline-none focus:border-blue-900 focus:ring focus:ring-blue-300 disabled:opacity-25 transition">Ver
+                                                Todo</a>
                                         </th>
                                     </tr>
                                 </tbody>
