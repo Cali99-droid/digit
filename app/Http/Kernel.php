e89077999044c5774b457092a00fa9d68fd18062
Carlos Alvarado Robles <carlos2000emilioa@gmail.com>
2022-12-16 19:36:16 -0500

    Corrigiendo mensajes de validación y añadiendo restricciones para las rutas de la ficha Socioeconómica

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'digitador_ficha_socioeconomica' => \App\Http\Middleware\FichaSocioeconomicaMiddleware::class,
     ];
 }
