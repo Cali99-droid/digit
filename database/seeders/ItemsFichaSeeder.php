2c53570246b12338427dffbafbb94c3df82bb4f3
Carlos Alvarado Robles <carlos2000emilioa@gmail.com>
2023-01-03 12:51:04 -0500

    Añadiendo los últimos Semestres Oficiales

--- database/seeders/ItemsFichaSeeder.php
+++ database/seeders/ItemsFichaSeeder.php
@@ -241,8 +241,24 @@ class ItemsFichaSeeder extends Seeder
             'nombre' => '2019-II'
         ]);
 
+        Semestres::create([
+            'nombre' => '2020-I'
+        ]);
+
+        Semestres::create([
+            'nombre' => '2020-II'
+        ]);
+
+        Semestres::create([
+            'nombre' => '2021-I'
+        ]);
+
+        Semestres::create([
+            'nombre' => '2021-II'
+        ]);
 
 
+        
         ClasificacionesSocioeconomicas::create([
             'nombre' => 'Pobre Extremo'
         ]);
